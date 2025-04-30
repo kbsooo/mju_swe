ebd59c41a12f049e7604e01a55dc28bc06e8dbf2
Kbyungs <kbsoo0620@icloud.com>
2025-04-30 13:19:33 +0900

    class structure setup

--- main.cpp
+++ main.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
+using namespace std;
+
+class FilesystemComponent
+{
+public:
+  FilesystemComponent() {}
+  virtual void display() = 0;
+};
+
+class File : public FilesystemComponent
+{
+public:
+  File() {}
+  void display() override
+  {
+    cout << "file display" << endl;
+  }
+};
+
+class Directory : public FilesystemComponent
+{
+public:
+  Directory() {}
+  void display() override
+  {
+    cout << "directory display" << endl;
+  }
+  void add() {}
+};
 
 int main()
 {
-  std::cout << "swe" << std::endl;
   return 0;
 }
