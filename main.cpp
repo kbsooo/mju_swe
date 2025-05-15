0054499933a6828a172182c7bbcd6958872d3122
Kbyungs <kbsoo0620@icloud.com>
2025-05-16 00:15:33 +0900

    delete lines

--- main.cpp
+++ main.cpp
@@ -97,35 +97,3 @@ int main() {
   delete root;
   return 0;
 }
-
-// int main() {
-//   // 현재 디렉터리 기준으로 파일 시스템 순회하며 FileSystemComponent 타입 객체를 만든다
-//   // <filesystem> 안의 기능 활용
-//   // 현재 디렉터리에 대응되는 Directory객체를 먼저 만든다
-//   // 현재 디렉터리 기준으로 파일 시스템을 순회하며
-//   // 파일을 만날 경우 이름과 크기 정보를 얻어서 File 객체를 만들어서 Directory 객체에 추가
-//   // 디렉터리를 만날 경우 DFS 방식으로 재귀적으로 처리한다
-//   // 즉 새 Directory 객체를 만들고 이를 현재 Directory 객체에 포함시킨다
-//   // 그리고 이 하위 디렉터리를 계속 순회하며 앞에 언급된 File과 Directory 객체 만드는 과정을 반복한다
-//   // 생성된 FilesystemComponent 타입 객체에 대해 (실제는 현재 디렉터리를 나타내는 Directory 객체) display() 메서드를 호출한다.
-//   // 현재 디렉터리에 대응되는 Directory의 display()가 호출될 것이므로 재귀적으롣 동작할 것
-//   // 디렉터리 깊이에 따라 들여쓰기 해준다
-
-//   FilesystemComponent *current = new Directory();
-
-//   for (현재 디렉터리부터 시작; 디렉터리 하위 항목이 있는 동안; 다음 하위 항목 읽기) {
-// 		if (디렉터리의 세부 항목이 파일이면) {
-// 			FilesystemComponent *file = new File(파일 이름, 파일 크기);
-// 			current->add(file);
-// 		} else if (디렉터리의 세부 항목이 디렉터리면) {
-// 			FilesystemComponent *dir = new Directory(디렉터리 이름);
-// 			current->add(dir);
-// 			// 새로 읽은 디렉터리에 대해서 재귀적으로 읽는 작업 적절히 수행하게 함
-// 		}
-// 	}
-	
-// 	// 이제 디렉터리의 모든 파일과 디렉터리를 읽었으므로 출력. 재귀적으로 동작할 것이다.
-// 	current->display();
-
-//   return 0;
-// }
